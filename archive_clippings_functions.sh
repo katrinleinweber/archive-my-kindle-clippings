b854c9e9dc870232b14a65aab1c94aae6787050e
Katrin Leinweber <9948149+katrinleinweber@users.noreply.github.com>
2020-01-04 15:23:57 +0100

    Extract functions (fix #1)

---
+++ archive_clippings_functions.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+extract_title() {
+    grep --invert-match --max-count=1 "^\s*$" "$1"
+}
+
+format_title() {
+    # Prepare title string as kebap-case-filename
+    echo "$1" | \
+        perl -pe 's/[[:punct:]]//g' | \
+        perl -pe 's/\W+/-/g' | \
+        perl -pe 's/-+$//' | \
+        perl -pe 's/\b$/.txt/'
+}
+
+extract_clips() {
+    grep --invert-match "$1" "$2"
+}
+
+format_clips() {
+    # Remove original separators & timestamps
+    # Convert descriptions into short ASCIDOC headings
+    perl -pe 's/^=+//g' "$1" | \
+    	perl -pe 's/ \|[ \wü]+,[ .\w]+(:\d+)+( \w+)?\s+//gi' | \
+    	perl -pe 's/- (Your|Ihre) /\r\n=== /gi'
+}
