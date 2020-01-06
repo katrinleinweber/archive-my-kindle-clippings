d3f1b8a168c33e1bef8ff1333859061ff8e86906
Katrin Leinweber <9948149+katrinleinweber@users.noreply.github.com>
2020-01-06 19:35:19 +0100

    Convert tabs to spaces

--- archive_clippings_functions.sh
+++ archive_clippings_functions.sh
@@ -21,6 +21,6 @@ format_clips() {
     # Remove original separators & timestamps
     # Convert descriptions into short ASCIDOC headings
     perl -pe 's/^=+//g' "$1" | \
-    	perl -pe 's/ \|[ \wü]+,[ .\w]+(:\d+)+( \w+)?\s+//gi' | \
-    	perl -pe 's/- (Your|Ihre) /\r\n=== /gi'
+        perl -pe 's/ \|[ \wü]+,[ .\w]+(:\d+)+( \w+)?\s+//gi' | \
+        perl -pe 's/- (Your|Ihre) /\r\n=== /gi'
 }
