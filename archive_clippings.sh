60b0706efc47b9b28225721e53844ad4223ee436
Katrin Leinweber <9948149+katrinleinweber@users.noreply.github.com>
2023-06-15 19:44:26 +0200

    Migrate to lefthook & apply GitLab's shfmt
    https://docs.gitlab.com/ee/development/shell_scripting_guide/

--- archive_clippings.sh
+++ archive_clippings.sh
@@ -11,42 +11,42 @@ cp "$CLIPS" "./_ori_clips-$(date -u +"%y%m%d-%H%M").txt"
 # learned on https://stackoverflow.com/a/7216394/4341322
 
 extract_title() {
-    grep --invert-match --max-count=1 "^\s*$" "$1"
+  grep --invert-match --max-count=1 "^\s*$" "$1"
 }
 
 format_title() {
-    # Prepare title string as kebap-case-filename
-    echo "$1" | \
-        perl -pe 's/\W+/-/g' | \
-        perl -pe 's/^-+//g' | \
-        perl -pe 's/\b(-+)?$/.txt/'
+  # Prepare title string as kebap-case-filename
+  echo "$1" |
+    perl -pe 's/\W+/-/g' |
+    perl -pe 's/^-+//g' |
+    perl -pe 's/\b(-+)?$/.txt/'
 }
 
 extract_clips() {
-    grep --invert-match "$1" "$2"
+  grep --invert-match "$1" "$2"
 }
 
 format_clips() {
-    # Remove original separators & timestamps
-    # Convert descriptions into short ASCIDOC headings
-    perl -pe 's/^=+//g' "$1" | \
-        perl -pe 's/ \|[^[:punct:]]+,[ .\w]+(:\d+)+( \w+)?\s+//gi' | \
-        perl -pe 's/- [A-Z][a-z]+ /\r\n== /g' | \
-        perl -pe 's/^== N/==== N/g'
+  # Remove original separators & timestamps
+  # Convert descriptions into short ASCIDOC headings
+  perl -pe 's/^=+//g' "$1" |
+    perl -pe 's/ \|[^[:punct:]]+,[ .\w]+(:\d+)+( \w+)?\s+//gi' |
+    perl -pe 's/- [A-Z][a-z]+ /\r\n== /g' |
+    perl -pe 's/^== N/==== N/g'
 }
 
 # Remove title & save clips into temp file
 TITLE=$(extract_title "$CLIPS")
-extract_clips "$TITLE" "$CLIPS" > "$TEMP"
+extract_clips "$TITLE" "$CLIPS" >"$TEMP"
 
 TITLE_F=$(format_title "$TITLE")
 touch "$TITLE_F"
-format_clips "$TEMP" >> "$TITLE_F"
+format_clips "$TEMP" >>"$TITLE_F"
 $EDITOR "$TITLE_F"
 
 # Clean up locally & on device
 trash "$TEMP"
-echo "" > "$CLIPS"
+echo "" >"$CLIPS"
 
 # Copy clippings onto device for review
 cp "$TITLE_F" "$DEV_PATH"
